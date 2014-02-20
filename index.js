e4f2baa2f5b5569bb3b270c277afc5149d743f41
melonHuang <melonh0327@gmail.com>
2014-02-20 17:26:52 +0800

    add hash and localStorage

--- index.js
+++ index.js
@@ -13,16 +13,46 @@ var URL = require('url');
 var express = require('express');
 var app = express();
 
+function queryUrl(url, key) {
+    url = url.replace(/^[^?=]*\?/ig, '').split('#')[0]; //去除网址与hash信息
+    var json = {};
+    //考虑到key中可能有特殊符号如“[].”等，而[]却有是否被编码的可能，所以，牺牲效率以求严谨，就算传了key参数，也是全部解析url。
+    url.replace(/(^|&)([^&=]+)=([^&]*)/g, function (a, b, key , value){
+        //对url这样不可信的内容进行decode，可能会抛异常，try一下；另外为了得到最合适的结果，这里要分别try
+        try {
+            key = decodeURIComponent(key);
+        } catch(e) {}
+
+        try {
+            value = decodeURIComponent(value);
+        } catch(e) {}
+
+        if (!(key in json)) {
+            json[key] = /\[\]$/.test(key) ? [value] : value; //如果参数名以[]结尾，则当作数组
+        }
+        else if (json[key] instanceof Array) {
+            json[key].push(value);
+        }
+        else {
+            json[key] = [json[key], value];
+        }
+    });
+    return key ? json[key] : json;
+}
+
 
 app.use(express.static(__dirname + '/web'));
 
 app.get('/proxy', function(req, res){
+    console.log('url:', req.url);
+    var hash = queryUrl(req.url, 'hash');
+    console.log('hash', hash);
 
     /**
      * inject script to original html
      */
     function injectScript(html) {
-        var clientScript = 'http://' + req.headers['host'].replace(/\:\d+/, '') + ':' + DEBUG_PORT + '/target/target-script-min.js#anonymous';
+        var clientScript = 'http://' + req.headers['host'].replace(/\:\d+/, '') + ':' + DEBUG_PORT + '/target/target-script-min.js#' + hash;
         return html.replace('</body>', '<script src="' + clientScript + '"></script></body>');
     }
 
--- web/js/index.js
+++ web/js/index.js
@@ -1,19 +1,31 @@
 (function(){
-
-    /* configuration */
-    var DEBUG_PORT = 8080;
-
     /* check browser support */
     if ($.Browser.ie && parseInt($.Browser.ie) < 9) {
         $('body').html('<div class="not-supported">请使用高级浏览器 ^_^</div>');
         return;
     }
 
+    /* configuration */
+    var DEBUG_PORT = 8080;
+    var storageKey = 'novakit';
+
     var pageTemplate = Handlebars.compile($('#page_template').html());
 
+
+    /* get and display storage data */
+    var pageNum = 0;
+    var urls = get(storageKey);
+    for(var i = urls.length - 1; urls && i >= 0 ; i--) {
+        !urls[i] && urls.splice(i, 1);
+    }
+    set(storageKey, urls);
+    for(var i = 0; urls && i < urls.length; i++) {
+        addPage(urls[i]);     
+    }
+
+    /* add page when submit */
     $('#add_page').submit(function(e){
         e.preventDefault();
-
         //check url
         var url = $('#input_url').val();
         if (!url || url.indexOf('http') != 0) {
@@ -21,13 +33,43 @@
             return;
         }
 
+        addPage(url);
+
+        // add to localStorage
+        var urls = get(storageKey) || [];
+        urls.push(url);
+        set(storageKey, urls);
+    });
+
+
+    /* remove page */
+    $('#page_list').delegate('.delete', 'click', function(e){
+        e.preventDefault();
+        var pageEl = $(this).closest('li');
+        var pageNum = pageEl.data('page-num');
+        pageEl.remove();
+        if ($('#page_list>li').length === 0) {
+            $('#hd').addClass('init-state');
+        }
+
+        var urls = get(storageKey) || [];
+        urls[pageNum] = undefined;
+        set(storageKey, urls);
+    });
+
+
+    function addPage(url) {
+        var hash = (new Date()).getTime();
+
         //add
         var pageHTML = pageTemplate({
+            pageNum: pageNum++, 
             url: url,
-            proxy_url: getProxyURL() + '?url=' + encodeURIComponent(url),
-            url_encoded: encodeURIComponent(getProxyURL() + '?url=' + encodeURIComponent(url)),
+            proxy_url: getProxyURL() + '?hash=' + hash + '&url=' + encodeURIComponent(url),
+            url_encoded: encodeURIComponent(getProxyURL() + '?hash=' + hash + '&url=' + encodeURIComponent(url)),
             host: location.host.replace(/\:\d+/, ''),
-            port: DEBUG_PORT
+            port: DEBUG_PORT,
+            hash: hash
         });
 
         //append to list
@@ -39,16 +81,7 @@
         //animate
         $('#hd').removeClass('init-state');
 
-    });
-
-    /* remove page */
-    $('#page_list').delegate('.delete', 'click', function(e){
-        e.preventDefault();
-        $(this).closest('li').remove();
-        if ($('#page_list>li').length === 0) {
-            $('#hd').addClass('init-state');
-        }
-    });
+    }
 
 
     /* utils */
@@ -59,6 +92,27 @@
         }
         url += '/proxy';
         return url;
+    }
+
+    // localStorage getter and setter
+    function set(key, val) {
+        if(val === undefined) {
+            window.localStorage.removeItem(key);
+            return;
+        }
+        window.localStorage.setItem(key, JSON.stringify(val));
+        return val;
+    }
+
+    function get(key) {
+        var val = window.localStorage.getItem(key);
+        if(typeof val !== 'string') {return undefined;}
+        try {
+            return JSON.parse(val);
+        }
+        catch(e) {
+            return val || undefined;
+        }
     }
 
 })();
