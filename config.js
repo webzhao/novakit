0870528648b10f0cbf78da573f8cef8c05e49f11
zhaowenbo <zhaowenbo@fe02v.add.zwt.qihoo.net>
2014-07-13 15:29:28 +0800

    修改novakit配置

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * novakit configuration
+ */
+module.exports = {
+
+    /* weinre server */
+    DEBUG_SERVER: 'http://novadebug.qiwoo.org',
+
+    /* weinre port */
+    DEBUG_PORT: 80,
+
+    /* novakit web port */
+    APP_PORT: 8866,
+
+};
